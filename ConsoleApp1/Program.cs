28ee210e4759747b02ab9c1f80a66aabe9c0e2a1
Ceydaay <cydylmz.99@gmail.com>
2024-07-15 17:53:34 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ceyda Yılmaz");
+            Console.WriteLine("harika deneyimler yaşayacağımı düşünüyorum ve yazılım dünyasında ilerlemek için güzel bir adım olarak görüyorum.");
+        }
+    }
+}
